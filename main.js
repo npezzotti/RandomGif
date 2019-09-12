debae7d6d521a067c7b8743f99cb8b6077a09280
npezzotti <npezzotti@hotmail.com>
2019-09-12 10:48:14 -0400

    Added onload function

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const url = "https://api.giphy.com/v1/gifs/search?"
 const apiKey = "api_key=Zw5WFdH43r4edQ8CgtZI453WmNqmVTef&q="
-const remainingUrl = "&limit=1&offset=0&rating=PG&lang=en"
+const limit = "&limit="
+const offset = "&offset="
+const remainingUrl = "&rating=PG&lang=en"
 const search = document.querySelector("#search");
 const heart = document.querySelector("#heart");
 const arrow = document.querySelector("#next");
@@ -8,10 +10,22 @@ const input = document.getElementById("search");
 const searchButton = document.querySelector("#search-button");
 const gif = document.querySelector("#gif");
 
+window.onload = randomGif();
+
+async function randomGif() {
+    let response = await axios.get("https://api.giphy.com/v1/gifs/random?api_key=Zw5WFdH43r4edQ8CgtZI453WmNqmVTef&tag=&rating=PG")
+    gif.src = response.data.data.images.fixed_width.url;
+}
+
 searchButton.addEventListener("click", getUrl);
+
+arrow.addEventListener("click", getUrl);
+
 
 async function getUrl() {
     event.preventDefault();
-    let response = await axios.get(url + apiKey + input.value + remainingUrl)
-    gif.src = response.data.data[0].images.downsized.url
+    let index = Math.floor(Math.random * 10); 
+    console.log(index)
+    let response = await axios.get(url + apiKey + input.value + limit + 10 + offset + index + remainingUrl)
+    gif.src = response.data.data[0].images.fixed_width.url
 }
