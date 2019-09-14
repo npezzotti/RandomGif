a642f9de8d0b06f441d44940d8daa1d437a7187b
npezzotti <npezzotti@hotmail.com>
2019-09-14 14:38:45 -0400

    Fixed footer, added better mobile compatibility

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const random = document.getElementById("random")
 const searchButton = document.querySelector("#search-button");
 const gif = document.getElementById("gif");
 let searchText = "";
+const mobile = window.matchMedia("(max-width: 900px)")
 
 window.onload = randomGif();
 
@@ -19,7 +20,12 @@ searchButton.addEventListener("click", getUrl);
 
 async function randomGif() {
     let response = await axios.get("https://api.giphy.com/v1/gifs/random?api_key=Zw5WFdH43r4edQ8CgtZI453WmNqmVTef&tag=&rating=PG")
-    gif.src = response.data.data.images.original.url;
+    if (mobile.matches) {
+        gif.src = response.data.data.images.fixed_width.url;
+    }
+    else {
+        gif.src = response.data.data.images.original.url;
+    }
     arrow.removeEventListener("click", trendingGif);
     arrow.removeEventListener("click", shuffle)
     arrow.addEventListener("click", randomGif);
@@ -32,7 +38,12 @@ async function getUrl() {
     input.value = "";
     let index = Math.floor((Math.random() * 50) + 1);
     let response = await axios.get(url + apiKey + searchText + limit + 1 + offset + index + remainingUrl);
-    gif.src = response.data.data[0].images.downsized.url;
+    if (mobile.matches) {
+        gif.src = response.data.data[0].images.fixed_width.url;
+    }
+    else {
+        gif.src = response.data.data[0].images.downsized.url;
+    }
     arrow.removeEventListener("click", trendingGif);
     arrow.removeEventListener("click", randomGif)
     arrow.addEventListener("click", shuffle);
@@ -41,12 +52,22 @@ async function getUrl() {
 async function shuffle() {
     let index = Math.floor((Math.random() * 50) + 1);
     let response = await axios.get(url + apiKey + searchText + limit + 1 + offset + index + remainingUrl);
-    gif.src = response.data.data[0].images.downsized.url;
+    if (mobile.matches) {
+        gif.src = response.data.data[0].images.fixed_width.url;
+    }
+    else {
+        gif.src = response.data.data[0].images.downsized.url;
+    }
 }
 
 async function trendingGif() {
     let response = await axios.get("https://api.giphy.com/v1/gifs/trending?api_key=Zw5WFdH43r4edQ8CgtZI453WmNqmVTef" + limit + 300 + remainingUrl);
-    gif.src = response.data.data[Math.floor(Math.random() * 300) + 1].images.original.url;
+    if (mobile.matches) {
+        gif.src = response.data.data[Math.floor(Math.random() * 300) + 1].images.fixed_width.url;
+    }
+    else {
+        gif.src = response.data.data[Math.floor(Math.random() * 300) + 1].images.original.url;
+    }
     arrow.removeEventListener("click", randomGif);
     arrow.removeEventListener("click", shuffle);
     arrow.addEventListener("click", trendingGif);
@@ -62,13 +83,6 @@ trending.addEventListener("click", trendingGif);
 function save() {
     localStorage.setItem(gif.src, localStorage.length);
     console.log("clicked", localStorage, Object.keys(localStorage));
-    // for (let i = 0; i <= localStorage.length; i++) {
-    //     if (Object.keys(localStorage).includes(`${i}`) == false) {
-    //         newKey = `${i}`;
-    //         window.localStorage.setItem(newKey, gif.src)
-    //         return;
-    //     }
-    // }
 }
 
 disk.addEventListener("click", displayStorage);
@@ -87,9 +101,6 @@ function displayStorage() {
         let cardImage = `<img src = "${image}" id ="gif-${index}" alt = "Gif">`;
         let node = document.getElementById("saved-gifs").insertAdjacentHTML('beforeend', cardImage);
     });
-//     index = array.indexOf(value);
-// if(index >= 0 && index < array.length - 1)
-//    nextItem = array[index + 1]
 }
 
 localStorage.clear();
